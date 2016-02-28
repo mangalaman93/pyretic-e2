f218443ba410c851bbf33107c26951054c67abe7
Aman Mangal <mangalaman93@gmail.com>
2016-02-27 21:03:15 -0500

    Create e2.py

---
+++ pyretic/e2/e2.py
@@ -0,0 +1,10 @@
+##########################################################
+# Computer Networks course project, CS6250, Georgia Tech #
+##########################################################
+
+"""E2 implementation using pyreric and mininet"""
+from pyretic.lib.std import *
+
+class e2():
+    def __init__(self, pol):
+        super(ft, self).__init__()
