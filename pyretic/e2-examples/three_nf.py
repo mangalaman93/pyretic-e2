6d9ba1b8ed6e07b97607f746f2a9feeb0ada7eda
sangeetha-jp <jsangeetha1104@gmail.com>
2016-03-11 21:07:10 -0500

    added example mininet class three_nf

--- pyretic/e2-examples/three_nf.py
+++ pyretic/e2-examples/three_nf.py
@@ -1,11 +1,9 @@
 ##########################################################
 # Computer Networks course project, CS6250, Georgia Tech #
 ##########################################################
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
-
 class Simple_topo(Topo):
     """
     A simple example topology class
@@ -27,10 +25,6 @@ class Simple_topo(Topo):
       +------+------+
              |
             ----
-            |s5|
-            ----
-             |
-            ----
             |s2|
             ----
              |
@@ -39,23 +33,20 @@ class Simple_topo(Topo):
             ----
     """
     def __init__(self):
-
-        Topo.__init__( self )
+        Topo.__init__(self)
         ###adding switches and hosts
         H1 = self.addHost('h1')
         H2 = self.addHost('h2')
-        Sw1 = self.addSwtich('s1')
-        Sw2 = self.addSwtich('s2')
-        Sw3 = self.addSwtich('s3')
-        Sw4 = self.addSwtich('s4')
-        Sw5 = self.addSwtich('s5')
+        Sw1 = self.addSwitch('s1')
+        Sw2 = self.addSwitch('s2')
+        Sw3 = self.addSwitch('s3')
+        Sw4 = self.addSwitch('s4')
         self.addLink( H1, Sw1 )
         self.addLink( H2, Sw2 )
         self.addLink( Sw1, Sw3 )
-        self.addLink( Sw3, Sw5 )
+        self.addLink( Sw3, Sw2 )
         self.addLink( Sw1, Sw4 )
-        self.addLink( Sw4, Sw5 )
-        self.addLink( Sw5, Sw2 )
+        self.addLink( Sw4, Sw2 )
 
 ##creating a mininet class
 net = Mininet( topo = Simple_topo, controller=lambda name: RemoteController( name='c0', ip='127.0.0.1',port=6633) )
@@ -63,3 +54,4 @@ net = Mininet( topo = Simple_topo, controller=lambda name: RemoteController( nam
 net.start()
 CLI(net)
 net.stop()
+
--- pyretic/e2/e2.py
+++ pyretic/e2/e2.py
@@ -9,14 +9,3 @@ import importlib
 class e2():
     def __init__(self, pol):
         super(ft, self).__init__()
-
-    def create_mininet_network(path_to_file,class_name):
-	#get mininet class from file
-        module = importlib.import_module(path_to_file)
-	mininet_class = getattr(module, class_name)
-	net = mininet_class()
-	#create the  topology
-        net.start()
-        CLI(net)
-        net.stop()
-        return mininet_class
