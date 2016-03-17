7fb40f5ad6a9c736aaf0f8cb4a8fc99a1614fe53
meerak <rhea@lawn-128-61-20-199.lawn.gatech.edu>
2016-03-17 16:24:11 -0400

    Initial placement

--- pyretic/e2-examples/pipelet-a.py
+++ pyretic/e2-examples/pipelet-a.py
@@ -42,13 +42,13 @@ from pyretic.e2.e2 import *
 net = Mininet( controller=RemoteController, link=TCLink, switch=OVSKernelSwitch )
 
 # Add hosts and switches
-internal_host = net.addHost('h1')
-external_host = net.addHost('h2')
-internal = net.addSwitch('s1')
-p = net.addSwitch('s2')
-n = net.addSwitch('s3')
-f = net.addSwitch('s4')
-external = net.addSwitch('s5')
+internal = net.addSwitch('s1', listenPort=7000, mac='00:00:00:00:00:01')
+p = net.addSwitch('s2',listenPort=7200, mac='00:00:00:00:00:01')
+n = net.addSwitch('s3',listenPort=7400, mac='00:00:00:00:00:01')
+f = net.addSwitch('s4',listenPort=7600, mac='00:00:00:00:00:01')
+external = net.addSwitch('s5',listenPort=7800, mac='00:00:00:00:00:01')
+internal_host = net.addHost('h1', mac='00:00:00:00:00:05', ip='10.0.0.5/8' )
+external_host = net.addHost('h2', mac='00:00:00:00:00:05', ip='10.0.0.5/8' )
 
 # Add links
 net.addLink(internal_host, internal)
@@ -63,20 +63,20 @@ c8 = net.addController( 'c8', controller=RemoteController, ip='127.0.0.1', port=
 net.start()
 
 # creating NFs
-internal = E2NF("internal", 1)
-p = E2NF("p", 1)
-n = E2NF("n", 1)
-f = E2NF("f", 1)
-external = E2NF("external", 1)
+internal_nf = E2NF("s1", 1)
+p_nf = E2NF("s2", 1)
+n_nf = E2NF("s3", 1)
+f_nf = E2NF("s4", 1)
+external_nf = E2NF("s5", 1)
 
 # adding filters
 fifa = E2Pipelet("pipelet-a")
-fifa.add_nodes_from([internal, p, n, f, external])
-fifa.add_edges_from([(internal, p, {'filter': match(dstport = 8000)}),
-    (internal, n, {'filter': ~match(dstport = 7000)}),
-    (p, n, {'filter': ~match(dstport = 8000)}),
-    (n, f, {'filter': match()}),
-    (f, external, {'filter': ~match(dstport = 8000)})])
+fifa.add_nodes_from([internal_nf, p_nf, n_nf, f_nf, external_nf])
+fifa.add_edges_from([(internal_nf, p_nf, {'filter': match(dstport = 7200)}),
+    (internal_nf, n_nf, {'filter': ~match(dstport = 7200)}),
+    (p_nf, n_nf, {'filter': ~match(dstport = 7200)}),
+    (n_nf, f_nf, {'filter': match()}),
+    (f_nf, external_nf, {'filter': ~match(dstport = 7200)})])
 
 def helper():
     dest = LoadGenerator.dest(8000)
@@ -86,4 +86,46 @@ def helper():
     internal_host.sendCmd(src_s2)
     external_host.sendCmd(dest)
 
+nodedict = {}
+
+# for edge in fifa.edges(data=True):
+#     if edge[0].name not in nodedict:
+#         nodedict[edge[0].name] = []
+
+#     nodedict[edge[0].name].append(edge)
+
+interfaces={}
+port={}
+for s in net.switches:
+    port[s.name] = s.listenPort
+#print port
+for link in net.links:
+    if link.intf1.name.split("-")[0] not in interfaces:
+        interfaces[link.intf1.name.split("-")[0]] = {}
+    pos = link.intf1.name.split("-")[1].rindex("h")+1
+    #print pos
+    interfaces[link.intf1.name.split("-")[0]][link.intf2.name.split("-")[0]]= link.intf1.name.split("-")[1][pos:]
+#print interfaces
+for edge in fifa.edges(data=True):
+     if edge[0].name not in nodedict:
+        nodedict[edge[0].name] = []
+     nodedict[edge[0].name].append(edge)
+#print nodedict
+allroutes = None
+for sw in port.keys():
+    route  =None
+    #print "Rule for ", sw 
+    if sw in nodedict:
+        for rule in nodedict[sw]:
+            if route is None:
+                route = rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name]) 
+            else:
+                route = route + rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name]) 
+        if allroutes is None:
+            allroutes = (match(srcport = port[sw]) >> route)
+        else:
+            allroutes += (match(srcport = port[sw]) >> route)
+    #print route
+print allroutes
+net.stop()
 e2(net, [fifa], helper).start()
