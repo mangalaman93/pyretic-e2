1823851f4a376f32d5e23f8d8eebd629983ddaf8
meerak <rhea@lawn-128-61-28-114.lawn.gatech.edu>
2016-03-25 11:44:10 -0400

    Fix e2 code

--- pyretic/e2/e2.py
+++ pyretic/e2/e2.py
@@ -5,52 +5,52 @@
 """E2 implementation using pyreric and mininet"""
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.link import Link, TCLink
 
 class e2():
     def __init__(self, net, pipelets, helper):
         self.net = net
-        self.piplets = pipelets
+        self.pipelets = pipelets[0]
         self.helper = helper
 
     def start(self):
-        return flood()
 
-# # for edge in fifa.edges(data=True):
-# #     if edge[0].name not in nodedict:
-# #         nodedict[edge[0].name] = []
+        nodedict = {}
+        interfaces={}
+        port={}
+        for s in self.net.switches:
+            port[s.name] = s.listenPort
 
-# #     nodedict[edge[0].name].append(edge)
+        for link in self.net.links:
+            if link.intf1.name.split("-")[0] not in interfaces:
+                interfaces[link.intf1.name.split("-")[0]] = {}
+            pos = link.intf1.name.split("-")[1].rindex("h")+1
+            interfaces[link.intf1.name.split("-")[0]][link.intf2.name.split("-")[0]] = link.intf1.name.split("-")[1][pos:]
 
-# interfaces={}
-# port={}
-# for s in net.switches:
-#     port[s.name] = s.listenPort
+        for edge in self.pipelets.edges(data=True):
+             if edge[0].name not in nodedict:
+                nodedict[edge[0].name] = []
+             nodedict[edge[0].name].append(edge)
 
-# for link in net.links:
-#     if link.intf1.name.split("-")[0] not in interfaces:
-#         interfaces[link.intf1.name.split("-")[0]] = {}
-#     pos = link.intf1.name.split("-")[1].rindex("h")+1
-#     interfaces[link.intf1.name.split("-")[0]][link.intf2.name.split("-")[0]] = link.intf1.name.split("-")[1][pos:]
-
-# for edge in fifa.edges(data=True):
-#      if edge[0].name not in nodedict:
-#         nodedict[edge[0].name] = []
-#      nodedict[edge[0].name].append(edge)
-
-# allroutes = None
-# for sw in port.keys():
-#     route  =None
-#     #print "Rule for ", sw
-#     if sw in nodedict:
-#         for rule in nodedict[sw]:
-#             if route is None:
-#                 route = rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name])
-#             else:
-#                 route = route + rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name])
-#         if allroutes is None:
-#             allroutes = (match(srcport = port[sw]) >> route)
-#         else:
-#             allroutes += (match(srcport = port[sw]) >> route)
-#     #print route
-# print allroutes
-# net.stop()
+        allroutes = None
+        for sw in port.keys():
+            route  =None
+            #print "Rule for ", sw
+            if sw in nodedict:
+                for rule in nodedict[sw]:
+                    if route is None:
+                        route = rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name])
+                    else:
+                        route = route + rule[2]['filter'] >> fwd(interfaces[sw][rule[1].name])
+                if allroutes is None:
+                    allroutes = (match(srcport = port[sw]) >> route)
+                else:
+                    allroutes += (match(srcport = port[sw]) >> route)
+            #print route
+        print allroutes
+        self.net.stop()
+        #return flood()
