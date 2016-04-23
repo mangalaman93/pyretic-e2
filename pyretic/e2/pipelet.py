d41fc28b3d3974c6d3a6849e33c9f1c292f533cd
meerak <meera.kamath12@gmail.com>
2016-04-23 18:32:01 -0400

    Update pipelet.py

--- pyretic/e2/pipelet.py
+++ pyretic/e2/pipelet.py
@@ -26,6 +26,6 @@ class E2NF(object):
 
 class E2Pipelet(nx.Graph):
     """E2 Pipelet defining end to end flow"""
-    def __init__(self, name):
+    def __init__(self, name=None):
         super(E2Pipelet, self).__init__()
         self.name = name
