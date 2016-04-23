c59395087e64ae6786c5079f218024f87bde8079
meerak <meera.kamath12@gmail.com>
2016-04-23 15:11:35 -0400

    Update e2.py

--- pyretic/e2/e2.py
+++ pyretic/e2/e2.py
@@ -156,7 +156,7 @@ class e2():
         print "Edges", igraph.edges()
 
         for src in list_of_srcs:
-            print "DFS EDGES", list(nx.dfs_edges(igraph,src))
+            print "DFS EDGES", src, list(nx.dfs_edges(igraph,src))
             for edge in list(nx.dfs_edges(igraph,src)):
                 print  "Edge", edge
                 if edge[1] in node_list:
