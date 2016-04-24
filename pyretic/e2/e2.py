f38749213ac9601cb64e041dfccc040832e7a0b5
Sneha <spisneha25@users.noreply.github.com>
2016-04-23 20:17:30 -0400

    Changed port to pid dict

--- pyretic/e2/e2.py
+++ pyretic/e2/e2.py
@@ -42,12 +42,12 @@ class e2():
     def policy(self):
         nodedict = {}
         interfaces = {}
-        port = {}
+        dpid = {}
         policy = None
         ipmap = {}
 
         for s in self.net.switches:
-            port[s.name] = s.dpid
+            dpid[s.name] = s.dpid
 
         for host in self.net.hosts:
             ipmap[host.name] = host.IP()
@@ -59,7 +59,7 @@ class e2():
             # if it is a host link
             if link.intf1.name.split("-")[0][0] == 'h':
                 host = link.intf1.name.split("-")[0]
-                rule = (match(switch = int(port[link.intf2.name.split("-")[0]]), dstip=ipmap[host]) >> fwd(int(link.intf2.name.split("-")[1][pos:])))
+                rule = (match(switch = int(dpid[link.intf2.name.split("-")[0]]), dstip=ipmap[host]) >> fwd(int(link.intf2.name.split("-")[1][pos:])))
                 if policy is None:
                     policy = rule
                 else:
@@ -67,7 +67,7 @@ class e2():
 
             if link.intf2.name.split("-")[0][0] == 'h':
                 host = link.intf2.name.split("-")[0]
-                rule = (match(switch = int(port[link.intf1.name.split("-")[0]]), dstip=ipmap[host]) >> fwd(int(link.intf1.name.split("-")[1][pos:])))
+                rule = (match(switch = int(dpid[link.intf1.name.split("-")[0]]), dstip=ipmap[host]) >> fwd(int(link.intf1.name.split("-")[1][pos:])))
                 if policy is None:
                     policy = rule
                 else:
@@ -85,7 +85,7 @@ class e2():
                 nodedict[edge[0].name] = []
              nodedict[edge[0].name].append(edge)
 
-        for sw in port.keys():
+        for sw in dpid.keys():
             route  = None
             if sw in nodedict:
                 for rule in nodedict[sw]:
@@ -94,9 +94,9 @@ class e2():
                     else:
                         route = route + (rule[2]['filter'] >> fwd(int(interfaces[sw][rule[1].name])))
                 if policy is None:
-                    policy = (match(switch = int(port[sw])) >> route)
+                    policy = (match(switch = int(dpid[sw])) >> route)
                 else:
-                    policy += (match(switch = int(port[sw])) >> route)
+                    policy += (match(switch = int(dpid[sw])) >> route)
         return policy
 
     def merge_pipelets(self, pipelets_list, graph_name):
